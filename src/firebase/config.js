f8edc0bc9c352461595dae0f421ebe713c54c41e
vivekreddy13 <vivekreddy2999@gmail.com>
2021-07-09 14:08:00 +0530

    Google Signin and Firebase Database

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export var firebaseConfig = {
+    apiKey: "AIzaSyBqvoIvfeKsZjiyXhTK7Z7R4Tphu60OyV4",
+    authDomain: "ruby-ecommerce.firebaseapp.com",
+    projectId: "ruby-ecommerce",
+    storageBucket: "ruby-ecommerce.appspot.com",
+    messagingSenderId: "116731105169",
+    appId: "1:116731105169:web:26d0215836d336657c5430"
+  };
