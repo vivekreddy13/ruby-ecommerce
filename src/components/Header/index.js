39889dc800b259eb0f41e848dc680fd2a200bb77
vivekreddy13 <vivekreddy2999@gmail.com>
2021-08-13 16:12:19 +0530

    Admin Products

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,73 +1,87 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { signOutUserStart } from './../../redux/User/user.actions';
-import './styles.scss'
-import { Link } from 'react-router-dom';
+import './styles.scss';
 
 import Logo from './../../assets/logo.png';
 
 const mapState = ({ user }) => ({
-    currentUser: user.currentUser
+  currentUser: user.currentUser
 });
 
 const Header = props => {
-    const dispatch = useDispatch();
-    const { currentUser } = useSelector(mapState);
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector(mapState);
 
-    const signOut = () => {
-        dispatch(signOutUserStart());
-    };
-    
-    return (
-        <header className="header">
-            <div className="wrap">
-                <div className="logo">
-                    <Link to="/">
-                        <img src={Logo} alt="RUBY LOGO" />
-                    </Link>
-                </div>
+  const signOut = () => {
+    dispatch(signOutUserStart());
+  };
 
-                <div className="callToActions">
+  return (
+    <header className="header">
+      <div className="wrap">
+        <div className="logo">
+          <Link to="/">
+            <img src={Logo} alt="SimpleTut LOGO" />
+          </Link>
+        </div>
 
-                    {currentUser && (
-                        <ul>
-                            <li>
-                                <Link to="/dashboard">
-                                    My Account
-                                </Link>
-                            </li>
-                            <li>
-                                <span onClick={() => signOut()}>
-                                    LOGOUT
-                                </span>
-                            </li>
-                        </ul> 
-                    )}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/search">
+                Search
+              </Link>
+            </li>
+          </ul>
+        </nav>
 
-                    {!currentUser && (
-                       <ul>
-                        <li>
-                            <Link to="/registration">
-                                Register
-                            </Link>
-                        </li>
-                        
-                        <li>
-                            <Link to="/login">
-                                Login
-                            </Link>
-                        </li>
-                    </ul> 
-                    )}
-                    
-                </div>
-            </div>
-        </header>
-    );
+        <div className="callToActions">
+
+          {currentUser && (
+            <ul>
+              <li>
+                <Link to="/dashboard">
+                  My Account
+                </Link>
+              </li>
+              <li>
+                <span onClick={() => signOut()}>
+                  LogOut
+                </span>
+              </li>
+            </ul>
+          )}
+
+          {!currentUser && (
+            <ul>
+              <li>
+                <Link to="/registration">
+                  Register
+              </Link>
+              </li>
+              <li>
+                <Link to="/login">
+                  Login
+              </Link>
+              </li>
+            </ul>
+          )}
+
+        </div>
+      </div>
+    </header>
+  );
 };
 
 Header.defaultProps = {
-    currentUser: null
+  currentUser: null
 };
 
 export default Header;
